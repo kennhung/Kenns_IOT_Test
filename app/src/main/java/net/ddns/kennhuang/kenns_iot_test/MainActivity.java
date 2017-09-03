c4bab23dd4992e8587fcd90d457c4c434273321b
kennhung <j20281@my.cmsh.cyc.edu.tw>
2017-09-03 19:14:23 +0800

    Initial commit

---
+++ app/src/main/java/net/ddns/kennhuang/kenns_iot_test/MainActivity.java
@@ -0,0 +1,13 @@
+package net.ddns.kennhuang.kenns_iot_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
